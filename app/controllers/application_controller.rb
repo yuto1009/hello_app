48d05cef92c07a214c282594746b5d54b9e7a5ee
yuto1009 <bmwb71009@gmail.com>
2023-10-22 23:42:32 +0900

    uploading 2023 Oct 22th Sun home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    def Hello
+        render html: "Hello, World!"
+    end
+    def Hola
+        render html: "¡Hola, mundo!"
+    end
+    def GoodBye
+        render html: "goodbye, world!"
+    end
 end
